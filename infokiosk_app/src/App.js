e9c39cc705733f22454017c15840697b62b38b61
Maamua06 <115535704+Maamua06@users.noreply.github.com>
2023-12-18 18:30:07 +0100

    Main

--- infokiosk_app/src/App.js
+++ infokiosk_app/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import Login from './Login';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     
+     <Login/>
     </div>
   );
 }
